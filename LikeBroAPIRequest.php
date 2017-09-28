dfb02e1b463b23b76ed3317ce09e39f0c49d7dba
Andrey Baranov <randebar@gmail.com>
2017-09-28 06:45:48 +0300

    append timeout in 2 seconds before CURL request

--- LikeBroAPIRequest.php
+++ LikeBroAPIRequest.php
@@ -3,6 +3,7 @@
   class LikeBroAPIRequest {
 
     public static function getBar($username) {
+        sleep(2);
       $ch = curl_init('http://cp.big-bro.pro/bars/' . $username);
       curl_setopt($ch, CURLOPT_POST, FALSE);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
